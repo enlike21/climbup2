482bda6f8482bdbdc4b7b2523df378bf07a61807
Enrique Velez <enriquevelezaguilar@gmail.com>
2025-04-24 02:23:39 +0200

    falta arreglar las migraciones

--- src/Controller/Location/MapController.php
+++ src/Controller/Location/MapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Location;
 
+use App\Entity\Location;
 use App\Repository\ClimbingRouteRepository;
 use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,24 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'map')]
-    public function index()
-    {
-        return $this->render('location/map.html.twig');
+    public function index(
+        LocationRepository $locationRepository,
+        ClimbingRouteRepository $climbingRouteRepository
+    ) {
+        $locations = $locationRepository->findAll();
+
+        $routes = $climbingRouteRepository->findAll();
+
+        return $this->render('location/map.html.twig', [
+            'locations' => $locations,
+            'routes' => $routes,
+        ]);
     }
 
     #[Route('/api/locations', name: 'api_locations')]
-    public function getLocations(LocationRepository $locationRepository): JsonResponse
+    public function apiLocations(LocationRepository $locationRepository): JsonResponse
     {
-        $locations = $locationRepository->findAll();
-        $data = array_map(fn($location) => [
-            'id' => $location->getId(),
-            'name' => $location->getName(),
-            'latitude' => $location->getLatitude(),
-            'longitude' => $location->getLongitude(),
-        ], $locations);
+        $data = $locationRepository->getAllLocationsWithRouteCount();
 
         return new JsonResponse($data);
     }
+
 
     #[Route('/api/routes', name: 'api_routes', methods: ['GET'])]
     public function getRoutes(ClimbingRouteRepository $routeRepository): JsonResponse
--- src/Repository/LocationRepository.php
+++ src/Repository/LocationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Location;
+use App\Entity\ClimbingRoute;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +17,45 @@ class LocationRepository extends ServiceEntityRepository
         parent::__construct($registry, Location::class);
     }
 
-    //    /**
-    //     * @return Location[] Returns an array of Location objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('l')
-    //            ->andWhere('l.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('l.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function getAllLocationsWithRouteCount(): array
+    {
+        // Corregir la consulta utilizando la relación unidireccional
+        $qb = $this->createQueryBuilder('l')
+            ->leftJoin('App\Entity\ClimbingRoute', 'r', 'WITH', 'r.location = l.id') // Asegúrate de que la relación se hace a través de la clase ClimbingRoute
+            ->addSelect('COUNT(r.id) AS routeCount')
+            ->addSelect('l.id', 'l.name', 'l.latitude', 'l.longitude')
+            ->groupBy('l.id');
 
-    //    public function findOneBySomeField($value): ?Location
-    //    {
-    //        return $this->createQueryBuilder('l')
-    //            ->andWhere('l.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        $query = $qb->getQuery();
+        $results = $query->getResult();
+
+        return array_map(function ($row) {
+            return [
+                'id' => $row[0]->getId(),
+                'name' => $row[0]->getName(),
+                'latitude' => $row[0]->getLatitude(),
+                'longitude' => $row[0]->getLongitude(),
+                'routeCount' => (int) $row['routeCount'],
+                'predominantType' => $this->getPredominantTypeForLocation($row[0]),
+            ];
+        }, $results);
+    }
+
+    public function getPredominantTypeForLocation(Location $location): ?string
+    {
+        // Obtener el tipo predominante de las rutas para esta localización
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->select('r.routeType, COUNT(r.id) as count') // Cambié 'type' a 'routeType'
+        ->from('App\Entity\ClimbingRoute', 'r')
+            ->where('r.location = :location')
+            ->setParameter('location', $location)
+            ->groupBy('r.routeType') // Cambié 'r.type' a 'r.routeType'
+            ->orderBy('count', 'DESC')
+            ->setMaxResults(1);
+
+        $result = $qb->getQuery()->getOneOrNullResult();
+
+        return $result ? $result['routeType'] : null;
+    }
 }
